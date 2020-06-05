37abc7e232de4b9b2e87776cf82544f2978259d8
Jean-Charles Ekoue-Kouvahey <62528423+Jeanchichi@users.noreply.github.com>
2020-06-05 22:55:03 +0200

    Face detection software that works with open-cv

---
+++ Face-detection/main.py
@@ -0,0 +1,16 @@
+import cv2
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+img = cv2.imread(r'C\Jeceey\Desktop\profile.jpg')
+img = cv2.resize(img, (500, 500))
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+for (x, y, w,h) in faces:
+    cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+cv2.imshow('image', img)
+cv2.waitKey()
